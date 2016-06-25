6e9da42bbed10024c824c4e4eca827afa652f803
CoiroTomas <coirotomas@gmail.com>
2016-06-25 00:51:34 -0300

    TP terminado

--- src/Equipo.scala
+++ src/Equipo.scala
@@ -1,5 +1,3 @@
-
-
 class Equipo(val miembros: List[Personaje], val pozo: Double, val nombre: String) {
 
   def getLider(): Option[Personaje] = {
@@ -21,9 +19,17 @@ class Equipo(val miembros: List[Personaje], val pozo: Double, val nombre: String
     Some(miembros.maxBy(criterio(_)))
   }
 
-  def realizarMision(mision: Mision): Equipo = {
-    // Hacer cada tarea, devolver equipo cambiado
-    this
+  def realizarMision(mision: Mision): Option[Equipo] = {
+    mision.tareas.foldRight[Option[Equipo]](Some(this)){
+      (tarea, equipo) => equipo.flatMap { e =>
+        val heroeApto = e.miembros.maxBy { tarea.facilidadPara(_, e) }
+        if (tarea.facilidadPara(heroeApto, e) <= 0) {
+          None
+        } else {
+          Some(tarea.realizadaPor(heroeApto, e))
+        }
+      }
+    }.map { mision.recompensarEquipo(_) }
   }
 
   def elegirMision(taberna: Taberna, criterio: (Equipo, Equipo) => Boolean): Option[Mision] = {
@@ -31,20 +37,28 @@ class Equipo(val miembros: List[Personaje], val pozo: Double, val nombre: String
       return None
     }
     Some(taberna.misiones.sortWith { (mision1, mision2) =>
-      val equipo1 = this.realizarMision(mision1)
-      val equipo2 = this.realizarMision(mision2)
-      criterio(equipo1, equipo2)
+      val equipo1 = realizarMision(mision1)
+      val equipo2 = realizarMision(mision2)
+      equipo1 match {
+        case None => false
+        case Some(e1) => equipo2 match {
+          case None     => true
+          case Some(e2) => criterio(e1, e2)
+        }
+      }
+
     }.head)
   }
 
-  def entrenar(taberna: Taberna): Equipo = {
-    // Hacer todas las misiones
-    this
+  def entrenar(taberna: Taberna): Option[Equipo] = {
+    taberna.misiones.foldRight[Option[Equipo]](Some(this)) { (mision, equipo) =>
+      equipo.flatMap(_.realizarMision(mision))
+    }
   }
 
   def obtenerUnItem(item: Equipable): Equipo = {
     val mejorHeroe = miembros.filter { heroe => item.puedeUsarlo(heroe) }
-            .maxBy { heroe => heroe.diferenciaEnStatPrincipal(item) }
+      .maxBy { heroe => heroe.diferenciaEnStatPrincipal(item) }
     if (mejorHeroe.diferenciaEnStatPrincipal(item) <= 0) {
       new Equipo(miembros, pozo + item.valor, nombre)
     } else {
--- src/Mision.scala
+++ src/Mision.scala
@@ -8,3 +8,41 @@ trait Mision {
   def recompensarEquipo(equipo: Equipo): Equipo;
   
 }
+
+trait Tarea {
+  
+  def facilidadPara(heroe: Personaje, equipo: Equipo): Int
+  def realizadaPor(heroe: Personaje, equipo: Equipo): Equipo
+
+}
+
+trait TareaPremioGanarOro extends {
+  var oro: Int = 50
+  
+  def setOro(oro: Int) {
+    this.oro = oro
+  }
+  
+  def realizadaPor(heroe: Personaje, equipo: Equipo): Equipo = {
+    new Equipo(equipo.miembros, equipo.pozo + oro, equipo.nombre)
+  }
+}
+
+object PelearContraMonstruo extends TareaPremioGanarOro {
+  
+  def facilidadPara(heroe: Personaje, equipo: Equipo): Int = {
+    val liderGuerrero = equipo.getLider().map { h => h.trabajo == Guerrero }.getOrElse(false)
+    if (liderGuerrero) {
+      20
+    } else {
+      10
+    }
+  }
+  
+}
+
+object ForzarPuerta extends TareaPremioGanarOro {
+  def facilidadPara(heroe: Personaje, equipo: Equipo): Int = {
+    equipo.miembros.count { p => p.trabajo == Ladron } + heroe.getInteligencia()
+  }
+}
